791fd5df0b8e8b07ef77774283424b58f654644b
Stefan Dimitrov <54206891+StefanDimitrovDimitrov@users.noreply.github.com>
2021-08-18 09:34:54 +0300

    updates

--- Internship/settings.py
+++ Internship/settings.py
@@ -109,9 +109,9 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
+STATICFILES_DIRS = (
     BASE_DIR / "static",
-]
+)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = join(BASE_DIR, 'media/')
