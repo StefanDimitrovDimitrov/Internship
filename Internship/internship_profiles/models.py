8b3f8d3af30de4393470746e95d78f6964fa5a2e
Stefan Dimitrov <54206891+StefanDimitrovDimitrov@users.noreply.github.com>
2021-08-19 11:28:33 +0300

    updates

--- Internship/internship_profiles/models.py
+++ Internship/internship_profiles/models.py
@@ -26,8 +26,8 @@ class CandidateProfile(models.Model):
         default='InitialProfilePics/pic.png',
     )
 
-    CV = models.FileField(
-        upload_to='intern_cv',
+    CV = cloudinary_model.CloudinaryField(
+        resource_type='file',
         blank=True,
     )
 
